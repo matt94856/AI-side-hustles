c8b666d43f5432cc207ca44e8c82581c50f722f3
matt94856 <129871867+matt94856@users.noreply.github.com>
2025-04-02 23:49:51 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -409,9 +409,11 @@ function updateProgressBar() {
 }
 
 // Initialize Supabase client
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+
 const supabaseUrl = 'https://tdxpostwbmpnsikjftvy.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkeHBvc3R3Ym1wbnNpa2pmdHZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyMDk5MzAsImV4cCI6MjA1ODc4NTkzMH0.-_azSsbF2xre1qQr7vppVoKzHAJRuzIgHzlutAMtmW0'
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function savePurchaseToDatabase(tutorialId, type, transactionDetails) {
     try {
